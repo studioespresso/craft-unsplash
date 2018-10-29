61d846750f827075d9d7758c25efa0979a3d7ef1
Jan Henckens <jan@studioespresso.co>
2018-10-29 21:57:55 +0100

    Template cleanup

--- src/assetbundles/splashingimages/dist/js/SplashingImages.js
+++ src/assetbundles/splashingimages/dist/js/SplashingImages.js
@@ -1,36 +1,21 @@
-/**
- * Splashing Images plugin for Craft CMS
- *
- * Splashing Images JS
- *
- * @author    Studio Espresso
- * @copyright Copyright (c) 2017 Studio Espresso
- * @link      https://studioespresso.co
- * @package   SplashingImages
- * @since     1.0.0
- */
-
 $(document).ready(function () {
-    var grid = jQuery('#splashing-container').masonry({
-        itemSelector: '.splashing-image-grid',
-        columnWidth: '.splashing-image-grid',
-        percentPosition: true,
-        gutter: 20,
-        stagger: 2
+    var $grid = $('.splashing-container').imagesLoaded( function() {
+        // init Masonry after all images have loaded
+        $grid.masonry({
+            itemSelector: '.splashing-image-grid',
+            columnWidth: '.splashing-image-grid',
+            percentPosition: true,
+            gutter: 20,
+            stagger: 2
+        });
     });
-    grid.imagesLoaded().progress(function () {
-        grid.masonry();
-    });
-});
 
-$(document).ready(function ($) {
-    var container = $('#splashing-container');
-    $('div.splashing-image').click(function (e) {
-        var $element = $(this);
-        $element.parent().addClass('saving');
+    $('.splashing-image').click(function (e) {
+        var $image = $(this);
+        $image.parent().addClass('saving');
 
         payload = {
-            id: $element.data('id')
+            id: $image.data('id')
         }
         payload[window.csrfTokenName] = window.csrfTokenValue;
         $.ajax({
@@ -41,11 +26,11 @@ $(document).ready(function ($) {
             beforeSend: function () {
             },
             success: function (response) {
-                console.log(response);
-                $element.parent().removeClass('saving');
+                $image.parent().removeClass('saving');
                 Craft.cp.displayNotice(Craft.t('splashing-images', 'Image saved!'));
             },
             error: function (xhr, status, error) {
+                $image.parent().removeClass('saving');
                 Craft.cp.displayError(Craft.t('splashing-images', 'Oops, something went wrong...'));
             }
         });
